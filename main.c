f90927f0043ab4a9f7db5ff923213a0bdbbc948b
Iam-AbdulSamad5257 <mohdabdulsamad2000@gmail.com>
2024-03-06 23:32:34 +0530

    added game board and main menu

--- main.c
+++ main.c
@@ -1,9 +1,40 @@
 #include"stdio.h"
-
+#include"windows.h"
+void menu(){
+    system("cls");
+    printf("\t\t\tX - O - X - O - X - O - X - O - X - O - X - O - X - O - X - O - X - O - X - O\n");
+    printf("\t\t\t|                                                                           |\n");
+    printf("\t\t\tO       X - O - X - O - X - O - X - O - X - O - X - O - X - O - X - O       X\n");
+    printf("\t\t\t|       |                                                           |       |\n");
+    printf("\t\t\tX       O                WELCOME TO TIC - TAC - TOE                 O       O\n");
+    printf("\t\t\t|       |                                                           |       |\n");
+    printf("\t\t\tO       X                                                           O       X\n");
+    printf("\t\t\t|       |               ENTER \" S \" TO START THE GAME               |       |\n");
+    printf("\t\t\tX       O                                                           X       O\n");
+    printf("\t\t\t|       |               ENTER \" H \" GET HELP                        |       |\n");
+    printf("\t\t\tO       X                                                           O       X\n");
+    printf("\t\t\t|       |               ENTER \" Q \" QUIT THE GAME                   |       |\n");
+    printf("\t\t\tX       O                                                           X       O\n");
+    printf("\t\t\t|       |                                                           |       |\n");
+    printf("\t\t\tO       X - O - X - O - X - O - X - O - X - O - X - O - X - O - X - O       X\n");
+    printf("\t\t\t|                                                                           |\n");
+    printf("\t\t\tX - O - X - O - X - O - X - O - X - O - X - O - X - O - X - O - X - O - X - O\n");
+}
+void gameboard(){
+    char box [10][5] = {"1","2","3","4","5","6","7","8","9"};
+    printf("\t\t     |     |     \n");
+    printf("\t\t  %s  |  %s  |  %s  \n",box[0],box[1],box[2]);
+    printf("\t\t_____|_____|_____\n");
+    printf("\t\t     |     |     \n");
+    printf("\t\t  %s  |  %s  |  %s  \n",box[3],box[4],box[5]);
+    printf("\t\t_____|_____|_____\n");
+    printf("\t\t     |     |     \n");
+    printf("\t\t  %s  |  %s  |  %s  \n",box[6],box[7],box[8]);
+    printf("\t\t     |     |     \n");
+}
 int main() {
 
-    printf("hello this is abdul samad");
-    
+
     return 0;
     
-}
+}    
